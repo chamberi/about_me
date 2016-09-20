66ae510c5664232d20c0b31388b71a3424f0852d
Colin Lamont <colinlamont@gmail.com>
2016-09-20 12:15:33 -0700

    scaffold html, js, css files; delete old html file

---
+++ app.js
@@ -0,0 +1,9 @@
+var username = prompt('What is your name?');
+console.log('The username is ' + username);
+var city = prompt('Where do you live?');
+console.log('The city is ' + city);
+var transport = prompt('How did you get to CodeFellows? (Car, Bus, etc.)');
+console.log('The transport is ' + transport);
+var food = prompt('What did you have for lunch?');
+console.log('The food is ' + food);
+alert(username + ' came to CodeFellows from ' + city + ' by ' + transport + ' and had ' + food + ' for lunch today.')
