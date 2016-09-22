1ff576f9eb7a9077b1df902b855d6031dbdf5bc5
Colin Lamont <colinlamont@gmail.com>
2016-09-22 11:51:39 -0700

    renamed title of html, changed correctcounter to += in js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var highTechJobs = prompt('Have I worked in high-tech companies since the mid 90
 console.log('highTechJobs is: ' + highTechJobs);
 if (highTechJobs === 'YES' || highTechJobs === 'Y') {
   alert(username + ', you are correct! I\'ve worked at startups in Seattle since 1994.');
-  correctCounter++;
+  correctCounter += 1;
 } else if (highTechJobs === 'NO' || highTechJobs === 'N') {
   alert(username + ', you are wrong on this one. I\'ve been at high-tech firms in Seattle since 1994.');
 } else {
@@ -20,7 +20,7 @@ var everFired = prompt('Have I ever been fired from a job before?').toUpperCase(
 console.log('everFired is: ' + everFired);
 if (everFired === 'NO' || everFired === 'N') {
   alert(username + ', you are correct! I work hard and add value, wherever I\'ve gone.');
-  correctCounter++;
+  correctCounter += 1;
 } else if (everFired === 'YES' || everFired === 'Y') {
   alert(username + ', nope. My employers have enjoyed my hard work and inquisitive attitude.');
 } else {
@@ -31,7 +31,7 @@ var vic20 = prompt('I started coding on a VIC-20 in the early 80s?').toUpperCase
 console.log('vic20 is: ' + vic20);
 if (vic20 === 'YES' || vic20 === 'Y') {
   alert(username + ', you are correct! I used to write 1,000 line programs in BASIC.');
-  correctCounter++;
+  correctCounter += 1;
 } else if (vic20 === 'NO' || vic20 === 'N') {
   alert(username + ', you\'re off on this one. I started coding in BASIC when I was 10.');
 } else {
@@ -42,7 +42,7 @@ var productIdea = prompt('I conceived of and pitched a product idea to my CEO th
 console.log('q4 is: ' + productIdea);
 if (productIdea === 'NO' || productIdea === 'N') {
   alert(username + ', you are correct. My product idea, RealPlayer Plus, has only made $750 Million since launching in 1997.');
-  correctCounter++;
+  correctCounter += 1;
 } else if (productIdea === 'YES' || productIdea === 'Y') {
   alert(username + ', I wish. But unfortunately no. My product idea, RealPlayer Plus, only made $750 Million since launching in 1997.');
 } else {
@@ -53,7 +53,7 @@ var companyLaunch = prompt('I launched a company, raised a seed round, hired a t
 console.log('companyLaunch is: ' + companyLaunch);
 if (companyLaunch === 'YES' || companyLaunch === 'Y') {
   alert(username + ', you are correct. It was a fun experience building a company up from nothing. Alas, it never gained the revenue it needed, but I learned a lot.');
-  correctCounter++;
+  correctCounter += 1;
 } else if (companyLaunch === 'NO' || companyLaunch === 'N') {
   alert(username + ', you are wrong. I did launch it, but it eventually it petered out. But it was a great learning experience.');
 } else {
@@ -67,7 +67,7 @@ for (var i = 0; i < 4; i++) {
 //evaluate guesses against the correct answer
   if (wine === 1938) {
     alert('You are correct.');
-    correctCounter++;
+    correctCounter += 1;
     break;
   } else if (wine > 1938) {
     alert('You guessed too recent. It\'s an older vintage (ie. lower number). Guess again.');
@@ -99,7 +99,7 @@ for (var socGuesses = 0; socGuesses < 6; socGuesses++) {
 // evaluate flag
   if (flag === 1) {
     alert('Correct.');
-    correctCounter++;
+    correctCounter += 1;
     break;
   } else {
     alert('Nope. Try again.');
